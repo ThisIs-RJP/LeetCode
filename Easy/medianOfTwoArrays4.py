2d41220d83bcd310fda92e0396813a6f6833bc1d
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2023-10-12 10:12:59 +0100

    Create medianOfTwoArrays4.py

---
+++ Easy/medianOfTwoArrays4.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        both = sorted(nums1 + nums2)
+        if len(both) % 2 != 0:
+            return (both)[len(both) // 2]
+        else:
+            return ((both)[len(both) // 2 - 1] + (both)[len(both) // 2]) / 2a
