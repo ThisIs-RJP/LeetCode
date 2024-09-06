fa29f1987287e1e0091307cd8f2d0dc84efc9332
Rhodney James Paraiso <rjparaiso369@gmail.com>
2024-09-06 16:01:57 +0100

    Finished Number Complement (476)

---
+++ Easy/Python/numComplement476.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def findComplement(num):
+
+    return str("{0:b}".format(5)).replace("0", ".").replace("1", "0").replace(".", "0")
--- Easy/Python/playground.py
+++ Easy/Python/playground.py
@@ -1,25 +1,4 @@
-def robotSim(commands, obstacles):
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    x, y = 0, 0
-    direction = 0
-    max_distance = 0
-    
-    obstacle_set = set(map(tuple, obstacles))
-    print(obstacle_set)
-    
-    for command in commands:
-            direction = (direction - 1) % 4
-            direction = (direction + 1) % 4
-            for _ in range(command):
-                nx, ny = x + directions[direction][0], y + directions[direction][1]
-                if (nx, ny) not in obstacle_set:
-                    x, y = nx, ny
-                    max_distance = max(max_distance, x*x + y*y)
-                else:
-                    break
-    
-    return max_distance
+#!/usr/bin/env python3
 
-print(robotSim([4, -1, 3], []))  # Expected output: 25
-print(robotSim([4, -1, 4, -2, 4], [[2, 4]]))  # Expected output: 65
-print(robotSim([6, -1, -1, 6], []))  # Expected output: 36
+# print(str("{0:b}".format(5)))
+print(int(str("{0:b}".format(5)).replace("0", ".").replace("1", "0").replace(".", "1"), 2))
